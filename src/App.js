616b31f732a07a1174abf91aa1fe5e7d3f23bd11
Vijay Kaushik P <vijaykaushik9911@gmail.com>
2025-04-15 00:28:08 +0530

    small ui changes

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
         {isLoading ? <Loader /> : (
           <>
             <FactList facts={facts} setFacts={setFacts} />
-
+          
             <AnimatePresence>
               {showStats && (
                 <>
